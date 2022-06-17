d9a28e60f961173215cb34467b4cab73adc95264
Krystle5183 <96323148+Krystle5183@users.noreply.github.com>
2022-06-17 13:27:04 -0500

    Add files via upload

---
+++ math.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+    return(a + b)
+}
+
+const difference = (a, b) => {
+    return(a - b)
+}
+
+const product = (a, b) => {
+    return(a * b)
+}
+
+const dividend = (a, b) => {
+    return(a / b)
+}
+
+module.exports = {
+    sum,
+    product,
+    difference,
+    dividend
+}
